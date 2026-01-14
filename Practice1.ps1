26c36d85b7e9b446607fcc5732329d4414edcc1c
Dmytro Malenyk <dmytro.malenyk@voco.ee>
2026-01-14 15:45:28 +0200

    Add Practice 1 and Practice2

---
+++ Practice1.ps1
@@ -0,0 +1,24 @@
+﻿#Array1 and Array2 
+$Array1 = @(1,2,3)
+$Array2 = @(4,5,6)
+
+#Array3 = sum of corresponding values
+$Array3 = @(
+    $Array1[0] + $Array2[0]
+    $Array1[1] + $Array2[1]
+    $Array1[2] + $Array2[2]
+)
+
+#Output
+Write-Output "Array1: $Array1"
+Write-Output "Array2: $Array2"
+Write-Output "Array3: $Array3"
+
+#Variant autori
+$array1 = @(1,2,3)
+$array2 = @(4,5,6)
+$array3 = @()
+$array3 += $array1[0] + $array2[0]
+$array3 += $array1[1] + $array2[1]
+$array3 += $array1[2] + $array2[2]
+$array3
---
+++ Practice2.ps1
@@ -0,0 +1,57 @@
+﻿$Hashtable1=@{Name=@("John","Joe","Mary");DaysWorked=@(12,20,18)}
+$Hashtable2=@{Name=@("John","Joe","Mary");SalaryPerDay=@(100,120,150)}
+$Hashtable3=@{
+    Name=$Hashtable1.Name;
+    Salary=@(
+        $Hashtable1.DaysWorked[0] * $Hashtable2.SalaryPerDay[0]
+        $Hashtable1.DaysWorked[1] * $Hashtable2.SalaryPerDay[1]
+        $Hashtable1.DaysWorked[2] * $Hashtable2.SalaryPerDay[2]
+        )
+}
+
+
+Write-Output $Hashtable3
+Write-Output $Hashtable3.Name
+Write-Output $Hashtable3.Salary
+
+
+#Variant1 autori
+$hash1 = @{Name="John","Joe","Mary";DaysWorked=12,20,18}
+$hash2 = @{Name="John","Joe","Mary";SalaryPerDay=100,120,150}
+$JohnSalary = $hash1.DaysWorked[0] * $hash2.SalaryPerDay[0]
+$JoeSalary = $hash1.DaysWorked[1] * $hash2.SalaryPerDay[1]
+$MarySalary = $hash1.DaysWorked[2] * $hash2.SalaryPerDay[2]
+$hash3 = @{
+    Name=$hash1.Name[0],$hash1.Name[1],$hash1.Name[2];
+    Salary=$JohnSalary,$JoeSalary,$MarySalary}
+$hash3
+
+#Variant2 autori. Alternate way using [PSCustomObject]
+$ht1 = [PSCustomObject]@{
+Name = "John","Joe","Mary"
+DaysWorked = 12,20,18
+}
+$ht2 = [PSCustomObject]@{
+Name = "John","Joe","Mary"
+SalaryPerDay = 100,120,150
+}
+$ht3 = @()
+$sal1 = $ht1.DaysWorked[0] * $ht2.SalaryPerDay[0]
+$sal2 = $ht1.DaysWorked[1] * $ht2.SalaryPerDay[1]
+$sal3 = $ht1.DaysWorked[2] * $ht2.SalaryPerDay[2]
+$ht = [PSCustomObject]@{
+Name = $ht1.Name[0]
+Salary = $sal1
+}
+$ht3 += $ht
+$ht = [PSCustomObject]@{
+Name = $ht2.Name[1]
+Salary = $sal2
+}
+$ht3 += $ht
+$ht = [PSCustomObject]@{
+Name = $ht1.Name[2]
+Salary = $sal3
+}
+$ht3 += $ht
+$ht3
